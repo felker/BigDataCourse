c7865e6d90b78dba047ca09a7a5937001edb6236
Kyle Gerard Felker <kfelker@princeton.edu>
2017-12-13 11:48:51 -0500

    Update broken Slurm command

--- 1_TransformationsActions/slurm.cmd
+++ 1_TransformationsActions/slurm.cmd
@@ -4,9 +4,8 @@
 #SBATCH -t 00:05:00
 #SBATCH --ntasks-per-node 1
 #SBATCH --cpus-per-task 1
-#SBATCH --reservation bigdata_introduction
+#SBATCH --reservation SparkClass
 
-module load python
 module load spark/hadoop2.7/2.2.0
 spark-start
 echo $MASTER
